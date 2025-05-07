6e5e584f467d5c9dddbda9053b117dcf3f0ba039
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-07 19:04:12 +0100

    added state to disable button click and add hover effects

--- src/components/StyledButton.jsx
+++ src/components/StyledButton.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-
-const buttonDisabled = false;
-const buttonStyle = {
-    backgroundColor: buttonDisabled ? 'gray' : 'blue',
-    color: buttonDisabled ? 'darkgray' : 'white',
-    padding: '10px 20px',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: buttonDisabled ? 'not-allowed' : 'pointer',
-}
+import React, { useState } from 'react'
 
 
 const StyledButton = () => {
+
+    const [buttonDisabled, setButtonDisabled] = useState(false)
+    const [isHovered, setIsHovered] = useState(false);
+
+    const buttonStyle = {
+        backgroundColor: buttonDisabled ? 'gray' : isHovered ? 'darkblue' : 'blue',
+        color: buttonDisabled ? 'darkgray' : 'white',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: buttonDisabled ? 'not-allowed' : 'pointer',
+    };
+
+
+
+    function disableButton() {
+        setButtonDisabled((prevState) => !prevState);
+    }
+
     return (
-        <div style={{marginTop: '20px', marginLeft: '15px' }}>
+        <div style={{textAlign: 'center', color: 'red', backgroundColor: 'beige', marginTop: '20px', marginLeft: '15px' }}>
             <h1 style={{fontSize: '30px', fontFamily: 'serif'}}>This is a styled button</h1>
-            <button style={buttonStyle} disabled={buttonDisabled}>Button!</button>
+            <button onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)} 
+                    onClick={disableButton} style={buttonStyle} 
+                    disabled={buttonDisabled}
+            >Button!</button>
         </div>
     )
     }
