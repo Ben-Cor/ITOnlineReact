bcccd4bd18b1fd56e5bc1ddc32eba87273485ebd
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-07 18:23:08 +0100

    jobBoard component made and implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import ProfileCard from './components/ProfileCard'
+import JobBoard from './components/JobBoard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
       image={'src/assets/react.svg'} 
       bio={'I am an aspiring software engineer with a passion for building web applications. I love learning new technologies and improving my skills.'}
       />
+      <JobBoard />
     </>
   )
 }
--- src/components/JobBoard.jsx
+++ src/components/JobBoard.jsx
@@ -2,9 +2,20 @@ function JobBoard() {
     const jobCount = 10;
     const companyName = "Tech Corp";
 
+    const getJobMessage = () => {
+        if (5 > jobCount > 0) {
+            return `Jobs running today from bot: ${jobCount}`;
+        } else if (jobCount > 5) {
+            return `Lots of jobs today! Current availability is: ${jobCount}`;
+        } else {
+            return `No jobs to schedule today.`;
+        }
+    }
+
     return (
     <div>
-        
+        <h1 className="text-2xl">{companyName}</h1>
+        <p>{getJobMessage()}</p>
     </div>
     )
 }
