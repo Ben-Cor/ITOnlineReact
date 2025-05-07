cbfa094b0cf0e18d62651bc8b9cdef8b73885421
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-07 10:59:20 +0100

    basic header component formed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Test Heading</h1>
+      <Header />
     </>
   )
 }
