1e5c2a9445557c618dea474b962130f6828b6c93
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-08 09:42:39 +0100

    jobCounter message added for button press

--- src/components/JobCounter.jsx
+++ src/components/JobCounter.jsx
@@ -3,19 +3,23 @@ import { useState } from "react";
 function JobCounter() {
 
     const [jobCount, setJobCount] = useState(0);
+    const [jobMessage, setJobMessage] = useState("");
 
     function handleJobAdd() {
         setJobCount(jobCount + 1);
+        setJobMessage("Job added!");
     }
 
     function handleJobRemove() {
         if (jobCount > 0) {
             setJobCount(jobCount - 1);
+            setJobMessage("Job removed!");
         }
     }
 
     function handleJobReset() {
         setJobCount(0);
+        setJobMessage("Job count reset!");
     }
 
     return (
@@ -25,6 +29,7 @@ function JobCounter() {
             <button className="border-2 rounded-md p-2" onClick={handleJobAdd}>Add a Job</button>
             <button className="border-2 rounded-md p-2" onClick={handleJobRemove}>Remove a job</button>
             <button className="border-2 rounded-md p-2" onClick={handleJobReset}>Reset</button>
+            <p className="text-red-500">{jobMessage}</p>
         </div>
     )
 }
